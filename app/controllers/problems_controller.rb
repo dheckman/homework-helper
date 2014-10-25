08fee081b427067bd9c943cdd85e4dde5e271130
Jose <jrchg1403@gmail.com>
2014-10-25 15:27:36 -0400

    implementing vote model

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,13 +1,13 @@
 class ProblemsController < ApplicationController
-  
+
   def index
     @problems = Problem.order(created_at: :asc)
   end
-  
+
   def new
     @problem = Problem.new
   end
-  
+
   def create
     @problem = current_user.problems.build(problem_params)
     if @problem.save
@@ -20,9 +20,18 @@ class ProblemsController < ApplicationController
   def show
     @problem = Problem.find(params[:id])
   end
-  
+
+  def vote
+    @vote = @question.votes.build(user: current_user, value: params[:vote][:value])
+    if @vote.save
+      redirect_to @question, success: "Your vote was recorded."
+    else
+      redirect_to @question, error: "There was a problem saving your vote."
+    end
+  end
+
   private
-  
+
   def problem_params
     params.require(:problem).permit(:text, :tried)
   end
