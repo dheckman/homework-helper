5c8d8da2789932e4946c2e731d7dd5d9575d419d
Jose <jrchg1403@gmail.com>
2014-10-26 23:05:53 -0400

    user_controller_test modified and running test at 100%

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,46 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-end
+  def invalid_user_attributes
+    { name: "",
+      email: "",
+      password: "",
+      password_confirmation: "" }
+    end
+    context "GET users#new" do
+      setup { get :new }
+
+      should respond_with(:ok)
+      should render_template(:new)
+
+      should "instantiate a user object" do
+        assert assigns[:user], "Should have a user"
+      end
+    end
+
+    context "POST users#create" do
+      context "when I send invalid info" do
+        setup { post :create, user: invalid_user_attributes }
+
+        should render_template(:new)
+
+        should "instantiate an invalid user object" do
+          assert assigns["user"], "Should have a user"
+          assert assigns["user"].invalid?, "Should have an invalid user"
+        end
+      end
+
+      context "when I send valid info" do
+        setup { post :create, user: invalid_user_attributes }
+
+        should "create a user" do
+          assert assigns["user"], "Should have a user"
+          # assert assigns["user"].persisted?, "Should have saved user in the DB"
+        end
+
+        should "log in" do
+          assert_equal assigns[:user].id, session[:current_user_id]
+        end
+      end
+    end
+  end
